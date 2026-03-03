582b8f1668c85392d6b41ce2a510c6db05bde0a1
Henry <webcatcat5@gmail.com>
2026-03-03 17:12:40 -0300

    adicionando as funcionalidades minimas

--- src/conversor.ts
+++ src/conversor.ts
@@ -2,27 +2,31 @@ import promptSync from "prompt-sync"
 const prompt = promptSync()
 
 let op: string
+let cotacao: number = 5.0
 
-let cotação: number = 5.0
-
-do{
+do {
     console.log("OPÇÕES:")
-    console.log("[1] - Converter BRL para USD")
-    console.log("[2] - Converter USD para BRL")
-    console.log("[3] - Alterar a cotação do dólar")
-    console.log("[0] - Sair")
-    op = prompt("Desejo a opção:")
+    console.log("[1] - Converter BRL para USD.")
+    console.log("[2] - Converter USD para BRL.")
+    console.log("[3] - Alterar a cotação do dólar.")
+    console.log("[0] - Sair.")
+    op = prompt("Opção:")
     switch(op){
         case '1':{
-            console.log("[1]")
+            let vBRL: number = Number(prompt("Valor em BRL:"))
+            let vUSD: number = vBRL / cotacao
+            console.log(`o valor em USD é: ${vUSD}`)
             break
         }
         case '2':{
-            console.log("[2]")
+            let vUSD: number = Number(prompt("Valor em USD:"))
+            let vBRL: number = vUSD * cotacao
+            console.log(`o valor em BRL é: ${vBRL}`)
             break
         }
         case '3':{
-            console.log("[3]")
+            cotacao = Number(prompt("A nova cotação será:"))
+            console.log(`A nova cotação do dólar é: ${cotacao}`)
             break
         }
         case '0':{
@@ -32,4 +36,4 @@ do{
             console.log("Opção invalida")
         }
     }
-}while(op!='0')
+} while(op!='0')
